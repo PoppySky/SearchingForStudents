e0238a6e8b329251293cbdb2542b8f73838a915f
Filip Pawelec <fpawelec0@gmail.com>
2022-09-19 21:14:32 +0200

    repaired mmultiline textbox

--- SearchingForStudents/Form2.Designer.cs
+++ SearchingForStudents/Form2.Designer.cs
@@ -127,7 +127,6 @@
             // 
             // textBox2
             // 
-            this.textBox2.Enabled = false;
             this.textBox2.Location = new System.Drawing.Point(10, 170);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
@@ -149,7 +148,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(99, 115);
+            this.label2.Location = new System.Drawing.Point(62, 56);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 17);
             this.label2.TabIndex = 0;
@@ -159,7 +158,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(99, 175);
+            this.label3.Location = new System.Drawing.Point(62, 116);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 17);
             this.label3.TabIndex = 1;
@@ -169,7 +168,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(99, 242);
+            this.label4.Location = new System.Drawing.Point(62, 183);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(48, 17);
             this.label4.TabIndex = 2;
@@ -177,28 +176,28 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(234, 115);
+            this.textBox3.Location = new System.Drawing.Point(197, 56);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(265, 20);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(234, 174);
+            this.textBox4.Location = new System.Drawing.Point(197, 115);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(265, 20);
             this.textBox4.TabIndex = 4;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(234, 241);
+            this.textBox5.Location = new System.Drawing.Point(197, 182);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(265, 20);
             this.textBox5.TabIndex = 5;
             // 
             // button_AddStudent
             // 
-            this.button_AddStudent.Location = new System.Drawing.Point(435, 313);
+            this.button_AddStudent.Location = new System.Drawing.Point(398, 254);
             this.button_AddStudent.Name = "button_AddStudent";
             this.button_AddStudent.Size = new System.Drawing.Size(127, 29);
             this.button_AddStudent.TabIndex = 6;
